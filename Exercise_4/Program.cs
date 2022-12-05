a4f7a47f2112dfd552450dc076e0bb7e949ec02b
Anisa Fauziah <anisa.fauziah.ft21@mail.umy.ac.id>
2022-12-05 15:39:52 +0700

    Add project files.

---
+++ Exercise_4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_4
+{
+    class node
+    {
+        public int info;
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
